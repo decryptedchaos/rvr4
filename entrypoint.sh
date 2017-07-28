886aad97832fc57c4790b88be40e807bf2dfcf40
Joshua Kerr <nixgod@gmail.com>
2017-07-27 20:44:47 -0500

    Attempt to detech arch

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,11 +20,13 @@ echo "~/server: ${MODIFIED_STARTUP}"
 
 cd /home/container/server
 
+BIT=getconf LONG_BIT
+
 # $NSS_WRAPPER_PASSWD and $NSS_WRAPPER_GROUP have been set by the Dockerfile
 export USER_ID=$(id -u)
 export GROUP_ID=$(id -g)
 envsubst < /passwd.template > ${NSS_WRAPPER_PASSWD}
-export LD_PRELOAD=/libnss_wrapper.so.64
+export LD_PRELOAD=/libnss_wrapper.so.${BIT}
 
 # Run the Server
 ${MODIFIED_STARTUP}
