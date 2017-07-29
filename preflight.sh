839540f5579df60499fed3ff27d9063b7c37730a
git <nixgod@gmail.com>
2017-07-29 10:49:34 -0500

    realign repo
    forgot escaping \

--- preflight.sh
+++ preflight.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-if [[ ! -d /home/container/.local ]]; then 
-	mkdir -p /home/container/.local/share
-	fi
 
-
-if [[ ! -d /home/container/cfg ]]; then 
-	mkdir -p /home/container/cfg
-	fi
 	
