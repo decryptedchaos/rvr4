4ff9e3dce28dc196b5cad79ef695a09905e10712
git <nixgod@gmail.com>
2017-07-29 21:44:38 -0500

    fix exacution for ash

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,9 @@ sleep 4
 #Install the Server
 if [[ ! -f /home/container/steam_appid.txt ]] || [[ ${UPDATE} == "1" ]]; then
 	if [[ -f /home/container/steam.txt ]]; then
-		/home/container/steamcmd/steamcmd.sh +login ${STEAM_USER} ${STEAM_PASS} +force_install_dir /home/container +app_update ${APP_ID} validate +runscript /home/container/steam.txt
+	. /home/container/steamcmd/steamcmd.sh +login ${STEAM_USER} ${STEAM_PASS} +force_install_dir /home/container +app_update ${APP_ID} validate +runscript /home/container/steam.txt
 	else
-		/home/container/steamcmd/steamcmd.sh +login ${STEAM_USER} ${STEAM_PASS} +force_install_dir /home/container +app_update ${APP_ID} validate +quit
+	. /home/container/steamcmd/steamcmd.sh +login ${STEAM_USER} ${STEAM_PASS} +force_install_dir /home/container +app_update ${APP_ID} validate +quit
 	fi
 fi
 
