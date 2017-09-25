96309db471c70d07a510f823f9b19b81eabdab79
decryptedchaos <nixgod@gmail.com>
2017-09-25 10:09:10 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,37 @@
 #!/bin/bash
-sleep 4
+sleep 2
+
 #Install the Server
-if [[ -f /home/container/steam_appid.txt ]] || [[ ${UPDATE} == "1" ]]; then
-	./home/container/steamcmd/steamcmd.sh +login ${STEAM_USER} ${STEAM_PASS} +force_install_dir /home/container +app_update ${APP_ID} validate +quit
+if [[ ! -d /home/container/server ]] || [[ ${UPDATE} == "1" ]]; then
+	if [[ -f /home/container/steam.txt ]]; then
+		/home/container/steamcmd/steamcmd.sh +login ${STEAM_USER} ${STEAM_PASS} +force_install_dir /home/container/server +app_update ${APP_ID} validate +runscript /home/container/steam.txt
+	else
+		/home/container/steamcmd/steamcmd.sh +login ${STEAM_USER} ${STEAM_PASS} +force_install_dir /home/container/server +app_update ${APP_ID} validate +quit
+	fi
 fi
 
 if [[ -f /home/container/preflight.sh ]]; then
 	/home/container/preflight.sh
 fi
- cd /home/container
 
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-
 echo "~/server: ${MODIFIED_STARTUP}"
+
+# Create some folders per the linux install guide
+mkdir -p /home/container/.local/share/Arma\ 3 && mkdir -p /home/container/.local/share/Arma\ 3\ -\ Other\ Profiles
+
+cd /home/container/server
+
+# Create symlink for MPMissions to solve random segfaults
+ln -s mpmissions MPMissions
+
+
+# $NSS_WRAPPER_PASSWD and $NSS_WRAPPER_GROUP have been set by the Dockerfile
+export USER_ID=$(id -u)
+export GROUP_ID=$(id -g)
+envsubst < /passwd.template > ${NSS_WRAPPER_PASSWD}
+export LD_PRELOAD=/libnss_wrapper.so
 
 # Run the Server
 ${MODIFIED_STARTUP}
