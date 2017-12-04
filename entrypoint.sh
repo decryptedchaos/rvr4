37013e3e832bd6c48026490594b6d1ac3838bd47
AutoJukebox <autojukebox@gmail.com>
2017-12-04 16:36:31 -0500

    Reverted

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,13 @@
 sleep 2
 
 #Install the Server
+if [[ ! -d /home/container/server ]] || [[ ${UPDATE} == "1" ]]; then
+	if [[ -f /home/container/steam.txt ]]; then
+		/home/container/steamcmd/steamcmd.sh +login ${STEAM_USER} ${STEAM_PASS} +force_install_dir /home/container/server +app_update ${APP_ID} validate +runscript /home/container/steam.txt
+	else
+		/home/container/steamcmd/steamcmd.sh +login ${STEAM_USER} ${STEAM_PASS} +force_install_dir /home/container/server +app_update ${APP_ID} validate +quit
+	fi
+fi
 
 if [[ -f /home/container/preflight.sh ]]; then
 	/home/container/preflight.sh
