7496c98c0864557c58b036419349c7a363b0c6d9
AutoJukebox <autojukebox@gmail.com>
2017-12-04 16:22:27 -0500

    Fixed Directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 sleep 2
 
+#Install the Server
+
 if [[ -f /home/container/preflight.sh ]]; then
 	/home/container/preflight.sh
 fi
@@ -12,10 +14,10 @@ echo "~/server: ${MODIFIED_STARTUP}"
 # Create some folders per the linux install guide
 mkdir -p /home/container/.local/share/Arma\ 3 && mkdir -p /home/container/.local/share/Arma\ 3\ -\ Other\ Profiles
 
-cd /home/container/
+cd /home/container/server
 
 # Create symlink for MPMissions to solve random segfaults
-mkdir -p /home/container/MPMissions
+mkdir -p /home/container/server/MPMissions
 
 
 # $NSS_WRAPPER_PASSWD and $NSS_WRAPPER_GROUP have been set by the Dockerfile
